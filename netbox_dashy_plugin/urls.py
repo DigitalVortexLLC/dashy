17d39e1ee84ff680d573235bd04386878734edbf
aroth01 <aroth01@gmail.com>
2025-03-24 20:08:33 -0600

    more work on ping widget

--- netbox_dashy_plugin/urls.py
+++ netbox_dashy_plugin/urls.py
@@ -5,15 +5,5 @@ from . import models, views
 
 
 urlpatterns = (
-    path("dashys/", views.DashyListView.as_view(), name="dashy_list"),
-    path("dashys/add/", views.DashyEditView.as_view(), name="dashy_add"),
-    path("dashys/<int:pk>/", views.DashyView.as_view(), name="dashy"),
-    path("dashys/<int:pk>/edit/", views.DashyEditView.as_view(), name="dashy_edit"),
-    path("dashys/<int:pk>/delete/", views.DashyDeleteView.as_view(), name="dashy_delete"),
-    path(
-        "dashys/<int:pk>/changelog/",
-        ObjectChangeLogView.as_view(),
-        name="dashy_changelog",
-        kwargs={"model": models.Dashy},
-    ),
+    path("ping/", views.Ping.as_view(), name="ping_widget"),
 )
--- netbox_dashy_plugin/views.py
+++ netbox_dashy_plugin/views.py
@@ -1,21 +1,31 @@
 from django.db.models import Count
 from django.contrib import messages
-from django.shortcuts import redirect
-from .jobs import PingJob
+from django.shortcuts import render
+from django.http import JsonResponse
+from .forms import PingForm
+from icmplib import ping
 from netbox.views import generic
 from . import filtersets, forms, models, tables
 
 
-class PingView(generic.ObjectView):
-    action = 'ping'
-    queryset = models.Dashy.objects.all()
-
-    def do_action(self, request, form):
-        PingJob.enqueue(
-            data=request.data,
-            user=request.user,
-            commit=False
-        )
-        messages.success(request, _("Pinging target IP"))
-
-        return redirect('home')
+def ping_view(request):
+    if request.method == 'POST':
+        form = PingForm(request.POST)
+        if form.is_valid():
+            target = form.cleaned_data['ip_address']
+            ping_result = ping(target, count=10, interval=0.2)
+            response = {
+                'avg_rtt': ping_result.avg_rtt,
+                'min_rtt': ping_result.min_rtt,
+                'max_rtt': ping_result.max_rtt,
+                'rtts': ping_result.rtts,
+                'sent': ping_result.packets_sent,
+                'recevied': ping_result.packets_received,
+                'packet_loss': ping_result.packet_loss,
+                'jitter': ping_result.jitter,
+                'is_alive': ping_result.is_alive
+            }
+            return JsonResponse(response)
+        else:
+            form = PingForm()
+        return render(request, 'ping_widget.html', {'form': form})
